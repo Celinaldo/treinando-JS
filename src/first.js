d95ba56cda7adabb51c62017440a119697873b2f
Celinaldo <celinaldoalvesferre@gmail.com>
2020-10-21 00:07:36 -0300

    projeto concluido

--- src/first.js
+++ src/first.js
@@ -1,34 +1,21 @@
 
-const calcular =  document.getElementById('calcular')
+const calcular =  document.getElementById('calcular');
 
 
 function imc(){
-    const nome =  document.getElementById('nome')
-    const altura =  document.getElementById('alt')
-    const peso =  document.getElementById('peso')
-    const resultado =  document.getElementById('resultado')
-
+    const nome =  document.getElementById('nome').value;
+    const altura =  document.getElementById('altura').value;
+    const peso =  document.getElementById('peso').value;
+    const resultado =  document.getElementById('resultado');   
+    
+    // ver se os campos estão preenchidos
     if (nome !== '' && altura !== ' ' && peso !== '') {
         const valorImc = (peso/(altura * altura)).toFixed(2);
         resultado.textContent = valorImc;
-
-        let classificacao = '';
-
-        if (valorImc<18.5) {
-            classificacao = 'abaixo do peso'
-            
-        }else if (valorImc<25){
-            classificacao = 'co peso ideal'
-        }else if (valorImc<30) {
-            classificacao = 'levemente acima do peso'
-            
-        }
-        resultado.textContent= `${nome} seu IMC é ${valorImc} e você ${classificacao}`
-        
     }else{
-        resultado.textContent = 'Preencha todos os campos'
-    
+        resultado.textContent = 'Preencha todos os valores do campo'
     }
+
 
 
 }
